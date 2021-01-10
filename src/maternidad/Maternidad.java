14e2957d0f9f05e1cdbd256bbadc514049c27d7f
USUARIO <USUARIO@DESKTOP-KOEI58B>
2021-01-09 21:59:57 -0500

    finalizacion de proyecto

--- src/maternidad/Maternidad.java
+++ src/maternidad/Maternidad.java
@@ -1,13 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Una clínica de maternidad necesita un sistema para determinar 
+cuántos bebés han nacido recientemente. Dicho sistema solicita 
+al usuario capturar la cantidad de nacimientos (N), el nombre de cada 
+bebé y su sexo (implemente un ciclo do-while para validar la captura,
+siendo M para masculino y F para femenino). Al final, debe imprimir 
+la cantidad de bebés de cada sexo.
  */
 package maternidad;
 
+import java.util.Scanner;
+
 /**
  *
- * @author USUARIO
+ * Josselyne ester chilito galindez, codigo: 20201187434
  */
 public class Maternidad {
 
@@ -15,7 +20,27 @@ public class Maternidad {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int Nb,f=0,m=0,c=1;
+        String genero,nombre;
+        System.out.println("señor usurio diguite el numero de bebes nacidos");
+        Nb=entrada.nextInt();
+        
+        do{System.out.println("nacimiento numero #"+c);
+            System.out.println("ingrese el genero del bebe");
+            System.out.println("si es femenino diguite la letra f ");
+            System.out.println("si es masculino diguite la letra m ");
+            genero = entrada.next();
+            
+            if("m".equals(genero)){
+            m=m+1;
+            }else{f=f+1;}
+            System.out.println("diguite el nombre del bebe");
+            nombre=entrada.next();
+            c=c+1;
+        }while (c<=Nb);
+                System.out.println("cantidad de nacidos del genero masculino #"+m);
+                System.out.println("cantidad de nacidos del genero femenino #"+f);
     }
     
 }
